03113d7edeec3843a8e0dd9601e0ed5552c0e52e
anidinri <anidinri@student.jyu.fi>
2024-11-05 12:28:36 +0200

    testit miinus viimeinen

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,33 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+        
+    #____________________________________________________________#
+    
+    def test_ottaa_liikaa(self):
+        self.varasto.ota_varastosta(11)
+        
+    def test_lisaa_liikaa(self):
+        self.varasto.lisaa_varastoon(9)
+        
+    def test_lisataan_alleO(self):
+        self.varasto.lisaa_varastoon(-2)
+        
+    def test_ota_alle0(self):
+        self.varasto.ota_varastosta(-10)
+        
+    def test_taynna(self):
+        self.varasto.lisaa_varastoon(12)
+        
+    def test_tilavuus_alle0(self):
+        self.varasto = Varasto(-1)
+        self.assertAlmostEqual(self.varasto.tilavuus, 0.0)
+        
+    def test_saldo_alle0(self):
+        self.varasto = Varasto(10, -3)
+        self.assertAlmostEqual(self.varasto.saldo, 0.0)
+        
+    def test_paljonko_tilaa(self):
+        self.uusivarasto = Varasto(10, 5)
+        self.uusivarasto("saldo = 5, vielä tilaa 5")
+    
